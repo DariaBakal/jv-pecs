ba27d152216311257b14f998946d4cb78abde931
DariaBakal <bakal.dasha01@gmail.com>
2024-11-04 17:07:57 +0200

    jv-pecs

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -4,7 +4,17 @@ package core.mate.academy.model;
  * Add some custom fields that could be only in Truck Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int cargoCapacity;
+
     public Truck() {
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
     }
 
     @Override
